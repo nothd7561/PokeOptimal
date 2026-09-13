1808be57deaf93811f80079ea4fc78d69499bf25
Lucas Lu <lucasxiao94@gmail.com>
2026-09-12 22:06:35 -0500

    Initial commit, reuploading project bit by bit with some changes

---
+++ data extraction/smogonv2.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import requests
+#pandas for df cleaning and manipulation
+#requestes for extracting data from the web
+
+###This script will pull data from recent 3 months of smogon competitive 
+###data and create a csv file for all the 3 months with the data in it.
+###want to make a df from the data then export that as a csv
+
+monthly_urls =['https://www.smogon.com/stats/2026-08/chaos/gen4ou-1500.json',
+               'https://www.smogon.com/stats/2026-07/chaos/gen4ou-1500.json',
+               'https://www.smogon.com/stats/2026-06/chaos/gen4ou-1500.json']
+#the urls for the smogon static files for the last 3 months of data
+
+def extract(link):
+### this function takes a link as an argument, then converts the json into a python dictionary. Then it extracts desired data and stores
+### each one into a variable, which then forms a dataframe by appending the variables (dictionaries) into an empty list. so the df is a list of dicts
+    extraction = requests.get(link)
+    #requests extract the info from the link and stores it in a variable
+    data = extraction.json()
+    #the data is in json format so we convert it to a python dictionary which can actually be modified
+    pokemon_data = data['data'] 
+    #specifically for smogon, the data we want is stored in the 'data' key of the dictionary, so we extract that and store it in a variable
+
+    desired_data = [] 
+    #an empty list to store our wanted data after extraction
+    for pokemon_name, stats in pokemon_data.items(): #for each pokemon, we extract name and stats
+        usage = stats['usage']
+        abilities = stats['Abilities']
+        items = stats['Items']
+        teammates = stats['Teammates']
+        moves = stats['Moves']
+        #we extract the data we want from the stats dictionary and store it in variables
+
+        desired_data.append({
+            'Pokemon': pokemon_name,
+            'Usage': usage,
+            'Abilities': abilities,
+            'Items': items,
+            'Teammates': teammates,
+            'Moves': moves
+        })
+        #we append the data we want to our empty list as a dictionary with the keys being the column names and the values being the data we extracted
+
+    smogon_frame = pd.DataFrame(desired_data) #makes a pandas df from the list of dictionaries
+    return smogon_frame #returns the df to be used later
+
+print(extract(f'https://www.smogon.com/stats/2026-06/chaos/gen4ou-1500.json')) #diagnose problem where i dont see all names of pokemon
