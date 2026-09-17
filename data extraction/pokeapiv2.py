67b47b9ec3007b71fc830cb3b3fa07a48410c286
Lucas Lu <lucasxiao94@gmail.com>
2026-09-16 22:42:47 -0500

    added type extraction, time delay to avoid hitting api limits

--- data extraction/pokeapiv2.py
+++ data extraction/pokeapiv2.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+import time
 
 
 csv_in = r'C:\Users\lucas\Downloads\personal coding\pokemon optimizer v2\data extraction\fetch_smogon.csv'
@@ -15,25 +16,37 @@ def extract_pokeapi(name):
     #lower the first letter of each pokemon name to match the api request format
 
     api_link = requests.get(f'https://pokeapi.co/api/v2/pokemon/{name}')
+    if api_link.json() == 200:
+        api_link = requests.get(f'https://pokeapi.co/api/v2/pokemon-species/{name}')
+    return api_link.json() #returns the json format for each pokemon, which is a dictionary with all the data we want to extract
     #for each name, put substitute it in the api link to get the data for that specific pokemon
 
 pokeapi_list = []
 #empty list to store the data we want from the api for each pokemon
 
 for name in names:
-    api_data =extract_pokeapi(name)
+    api_data = extract_pokeapi(name)
     #for each name in the list of names, call the function to extract the data from the api
-
-    print('Extracting data for:', name)
-    poke_dic = {
-        'Pokemon': name,
-        'HP': api_data['stats'][0]['base_stat'],
-        'Attack': api_data['stats'][1]['base_stat'],
-        'Defense': api_data['stats'][2]['base_stat'],
-        'Special Attack': api_data['stats'][3]['base_stat'],
-        'Special Defense': api_data['stats'][4]['base_stat'],
-        'Speed': api_data['stats'][5]['base_stat'],
-        #creating a dictionary with the data(stats) we want from the api for each pokemon,
-        #these lines are found from analyzing the json data from the api and finding where the data we want is stored
+    if api_data is not None:
+        print('Extracting data for:', name)
+        poke_dic = {
+            'Pokemon': name,
+            'HP': api_data['stats'][0]['base_stat'],
+            'Attack': api_data['stats'][1]['base_stat'],
+            'Defense': api_data['stats'][2]['base_stat'],
+            'Special Attack': api_data['stats'][3]['base_stat'],
+            'Special Defense': api_data['stats'][4]['base_stat'],
+            'Speed': api_data['stats'][5]['base_stat'],
+            #creating a dictionary with the data(stats) we want from the api for each pokemon,
+            #these lines are found from analyzing the json data from the api and finding where the data we want is stored
+            'Type1': api_data['types'][0]['type']['name'],
+            'Type2': api_data['types'][1]['type']['name'] if len(api_data['types']) > 1 else None
+            #if the pokemon has a second type, we extract it, if not we store None
     }
     
+
+    pokeapi_list.append(poke_dic)
+    #append the dictionary to the list for each pokemon, ending up with a list of dictionaries to set up for a dataframe
+
+pokeapi_df = pd.DataFrame(pokeapi_list)
+pokeapi_df.to_csv(r'C:\Users\lucas\Downloads\personal coding\pokemon optimizer v2\data extraction\fetch_pokeapi.csv', index=False)
